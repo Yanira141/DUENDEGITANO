fb7bd526b90a500bbab0d84eff221342445e43fd
Yanira141 <yaniragonru@gmail.com>
2023-02-21 15:16:02 +0100

    adfdadf

--- REACT/vite-project/src/views/GrupoDetalle/GrupoDetalle.jsx
+++ REACT/vite-project/src/views/GrupoDetalle/GrupoDetalle.jsx
@@ -4,57 +4,56 @@ import { useAuthContext } from "../../context/AuthContext/logInContext";
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-export default function GrupoDetalle (){
+export default function GrupoDetalle() {
   const [existeGrupo, setExisteGrupo] = useState(false);
   const { authorization } = useAuthContext();
   const [buttonDelete, setButtonDelete] = useState([]);
-    const [grupos, setGrupos] = useState(null);
-    const params = useParams();
-    
-    useEffect(function () {
-      async function fetchGrupos() {
-        const response = await fetch(
-          `http://localhost:3000/grupo/${params.idgrupo}`
-      
-        );
-        const detalles = await response.json();
-        setGrupos(detalles);
+  const [grupos, setGrupos] = useState(null);
+  const params = useParams();
+
+  useEffect(function () {
+    async function fetchGrupos() {
+      const response = await fetch(
+        `http://localhost:3000/grupo/${params.idgrupo}`
+      );
+      const detalles = await response.json();
+      setGrupos(detalles);
+    }
+
+    fetchGrupos();
+  }, []);
+
+  useEffect(function () {
+    const { idgrupo } = params;
+    async function fetchGrupos() {
+      const response = await fetch(
+        `http://localhost:3000/grupo/buttondeletegrupo/${idgrupo}/${authorization.id}`
+      );
+      if (response.status === 404) {
+        setExisteGrupo(false);
       }
-  
-      fetchGrupos();
-    }, []);
-    
-    useEffect(function () {
-      const { idgrupo } = params;
-      async function fetchGrupos() {
-        const response = await fetch(
-          `http://localhost:3000/grupo/buttondeletegrupo/${idgrupo}/${authorization.id}`
-      
-        );
-        if(response.status===404){
-          setExisteGrupo(false)
-        }
-     
-      
-        setExisteGrupo(true);
-      }
-  
-      fetchGrupos();
-    }, []);
-    
-    return(
-        <>
-        <Breadcrumbs title={"Detalles del grupo"} link={"Detalles del grupo"}/>
-       
-        <div className="pb-5" >
+
+      setExisteGrupo(true);
+    }
+
+    fetchGrupos();
+  }, []);
+
+  return (
+    <>
+      <Breadcrumbs title={"Detalles del grupo"} link={"Detalles del grupo"} />
+
+      <div className="pb-5">
         {grupos ? (
           <CardGrupoDetalle
-            grupos={grupos} setExisteGrupo={setExisteGrupo} existeGrupo={existeGrupo}
+            grupos={grupos}
+            setExisteGrupo={setExisteGrupo}
+            existeGrupo={existeGrupo}
           />
         ) : (
           <p>Cargando...</p>
         )}
       </div>
-        </>
-    )
+    </>
+  );
 }
